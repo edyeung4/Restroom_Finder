26bee556452c57fbcf2c17b9eb6b33e8492e99b4
Devin Barger <bargerd@flip2.engr.oregonstate.edu>
2021-02-23 21:05:21 -0800

    Updates to app.py for backend hookup
    Flask now queries the MariaDB instance on flip2 in order to
    retrieve data for the table displayed on employee_index.html.
    
    Print statements have also been added in temporarily for easier
    debugging of the query results to help with formatting Jinja template
    on the HTML side.

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, request, redirect
-import database.db_connector as db
+from database.db_connector import connect_to_database, execute_query
 import os
 
 # Configuration
@@ -30,8 +30,21 @@ def emp_index():
     '''Employee login landing page. Displays data from Restrooms, RestroomEmployees,
     and Locations'''
     print('Accessing the employee_index landing page.')
-
-    return render_template('employee_index.html')
+    db_connection = connect_to_database()
+    query = "SELECT rr.restroomID, concat(l.street, ', ', l.city, ', ', l.state, ', ', l.country ) as Address,rr.openHour, rr.closeHour, rr.free, re.inspectedAt, re.comments, re.employeeID FROM Restrooms rr JOIN Locations l on l.locationID = rr.locationID JOIN RestroomsEmployees re on re.restroomID = rr.restroomID ORDER BY 1 asc;"
+    results = execute_query(db_connection, query).fetchall()
+    for result in results:
+        print()
+        print(result[0])
+        print(result[1])
+        print(result[2])
+        print(result[3])
+        print(result[4])
+        print(result[5])
+        print(result[6])
+        print(result[7])
+        print()
+    return render_template('employee_index.html', results=results)
 
 
 
