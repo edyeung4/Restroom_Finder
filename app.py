d32afe9d92db651875c7cd743b24c3deded79f1d
Devin Barger <bargerd@flip2.engr.oregonstate.edu>
2021-02-24 18:29:05 -0800

    Add /employee_add route to app.py
    Added the /employee_add route to app.py in order to handle both
    the GET and POST HTTP methods for users navigating to /employee_add.
    
    If a GET request, then simply return the page to the user where they
    can fill out the form to add a new restroom.
    
    If a POST request (via submit button being pressed on the form),
    then collect the data provided and format into MySQL query to
    insert new Restroom and associated data into DB.

--- app.py
+++ app.py
@@ -33,22 +33,14 @@ def emp_index():
     db_connection = connect_to_database()
     query = "SELECT rr.restroomID, concat(l.street, ', ', l.city, ', ', l.state, ', ', l.country ) as Address,rr.openHour, rr.closeHour, rr.free, re.inspectedAt, re.comments, re.employeeID FROM Restrooms rr JOIN Locations l on l.locationID = rr.locationID JOIN RestroomsEmployees re on re.restroomID = rr.restroomID ORDER BY 1 asc;"
     results = execute_query(db_connection, query).fetchall()
-    for result in results:
-        print()
-        print(result[0])
-        print(result[1])
-        print(result[2])
-        print(result[3])
-        print(result[4])
-        print(result[5])
-        print(result[6])
-        print(result[7])
-        print()
     return render_template('employee_index.html', results=results)
 
-
-
-# Listener
+@app.route('/employee_add', methods=['GET','POST'])
+def emp_add():
+    '''Employee add page. If a GET request, then simply return the page to the user. If the page is reached via an HTTP POST, then collect the data provided and INSERT the new Restroom and appropriate data.'''
+    if request.method == 'POST':
+        pass
+    return render_template('employee_add.html')
 
 if __name__ == "__main__":
     port = int(os.environ.get('PORT', 56124)) 
